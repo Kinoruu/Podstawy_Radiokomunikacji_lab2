3b28f7792b736713d26d0524a9b273e711c09b93
Jakub Siejak <52972584+Kinoruu@users.noreply.github.com>
2020-11-09 02:55:18 +0100

    added constants to clean up code updated dirst part ended second part in second part display is not working for shadow

--- pr_lab_2.m
+++ pr_lab_2.m
@@ -1,13 +1,15 @@
+f = 3.6 * 10^9;      %czêstotliwoœæ
+c = 3 * 10^8;        %prêdkoœæ œwiat³a
 d_max = 2.8;         %d³ugoœæ pokoju
 s_max = 2.6;         %szerokoœæ pokoju
 h_max = 2.6;         %wysokoœæ pokoju
 step1 = 0.02;         %krok obliczeñ dla zadania 1
 step2 = 0.02;         %krok obliczeñ dla zadania 2
 d = [1:step1:d_max];    %zakres dla zadania 1
-%s = [0:step2:(2 * s_max)];    %zakres dla zadania 2
+sx = [0:step2:(2 * s_max)];    %zakres dla zadania 2
 a = -0.5;            %wspó³czynnik odbicia
 %%%%%%%%%%%%%%%%%  LOS  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-flos = -2 * pi * 3.6 * 10^9 * (d ./ (3 * 10^8));
+flos = -2 * pi * f * (d ./ c);
 
 prpo = 1 ./ d;
 
@@ -24,9 +26,9 @@ dsc = hypot(d, s_max);
 dsu = hypot(d, h_max);
 dscp = (2 * d_max) - d;
 
-fTXscRX = -2 * pi * 3.6 * 10^9 * (dsc ./ (3 * 10^8)); %œciana
-fTXsuRX = -2 * pi * 3.6 * 10^9 * (dsu ./ (3 * 10^8)); %sufit
-fTXscpRX = -2 * pi * 3.6 * 10^9 * (dscp ./ (3 * 10^8)); %œciana w linii LOS
+fTXscRX = -2 * pi * f * (dsc ./ c); %œciana
+fTXsuRX = -2 * pi * f * (dsu ./ c); %sufit
+fTXscpRX = -2 * pi * f * (dscp ./ c); %œciana w linii LOS
 
 prposc = ((a ./ dsc) .* exp(-j .* fTXscRX));
 prposu = ((a ./ dsu) .* exp(-j .* fTXsuRX));
@@ -47,9 +49,9 @@ dscsc = hypot(d, (2 * s_max)); %œciana œciana
 dsupo = hypot(d, (2 * h_max)); %sufit pod³oga / pod³oga sufit
 dscscp = (3 * d_max) - d; % œciana œciana w lini LOS
 
-fTXscscRX = -2 * pi * 3.6 * 10^9 * (dscsc ./ (3 * 10^8)); %œciana
-fTXsupoRX = -2 * pi * 3.6 * 10^9 * (dsupo ./ (3 * 10^8)); %sufit
-fTXscscpRX = -2 * pi * 3.6 * 10^9 * (dscscp ./ (3 * 10^8)); %œciana za plecami
+fTXscscRX = -2 * pi * f * (dscsc ./ c); %œciana
+fTXsupoRX = -2 * pi * f * (dsupo ./ c); %sufit
+fTXscscpRX = -2 * pi * f * (dscscp ./ c); %œciana za plecami
 
 prposcsc = (((a * a) ./ dscsc) .* exp(-j .* fTXscscRX));
 prposupo = (((a * a) ./ dsupo) .* exp(-j .* fTXsupoRX));
@@ -71,9 +73,9 @@ dscscsc = hypot(d, (3 * s_max)); %œciana œciana œciana
 dsuposu = hypot(d, (3 * h_max)); %sufit pod³oga sufit/ pod³oga sufit pod³oga
 dscscscp = (4 * d_max) - d; % œciana œciana œciana w lini LOS
 
-fTXscscscRX = -2 * pi * 3.6 * 10^9 * (dscscsc ./ (3 * 10^8)); %œciana
-fTXsuposcRX = -2 * pi * 3.6 * 10^9 * (dsuposu ./ (3 * 10^8)); %sufit
-fTXscscscpRX = -2 * pi * 3.6 * 10^9 * (dscscscp ./ (3 * 10^8)); %œciana za plecami
+fTXscscscRX = -2 * pi * f * (dscscsc ./ c); %œciana
+fTXsuposcRX = -2 * pi * f * (dsuposu ./ c); %sufit
+fTXscscscpRX = -2 * pi * f * (dscscscp ./ c); %œciana za plecami
 
 prposcscsc = (((a * a * a) ./ dscscsc) .* exp(-j .* fTXscscscRX));
 prposuposu = (((a * a * a) ./ dsuposu) .* exp(-j .* fTXsuposcRX));
@@ -108,7 +110,7 @@ legend('LOS','LOS + 1','LOS + 1 + 2','LOS + 1 + 2 + 3')
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 for n = 0:3
     if n == 0
-        flos = -2 * pi * 3.6 * 10^9 * (d ./ (3 * 10^8));
+        flos = -2 * pi * f * (d ./ c);
         prpolos = 1 ./ d;
         sumaprpo = prpolos;
     else
@@ -116,9 +118,9 @@ for n = 0:3
         dsun = hypot(d, (n * h_max));
         dscpn = abs(((n+1) * d_max) - d);
 
-        fTXscRXn = -2 * pi * 3.6 * 10^9 * (dscn ./ (3 * 10^8)); %œciana
-        fTXsuRXn = -2 * pi * 3.6 * 10^9 * (dsun ./ (3 * 10^8)); %sufit
-        fTXscpRXn = -2 * pi * 3.6 * 10^9 * (dscpn ./ (3 * 10^8)); %œciana w linii LOS
+        fTXscRXn = -2 * pi * f * (dscn ./ c); %œciana
+        fTXsuRXn = -2 * pi * f * (dsun ./ c); %sufit
+        fTXscpRXn = -2 * pi * f * (dscpn ./ c); %œciana w linii LOS
 
         prposcn = (((a^n) ./ dscn) .* exp(-j .* fTXscRXn));
         prposun = (((a^n) ./ dsun) .* exp(-j .* fTXsuRXn));
@@ -141,31 +143,61 @@ ylabel('Moc [dB]');
 %      czêœæ druga
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 prpo2 = [];
+absprpo2 = [];
 for s = 0:step2:(2 * s_max)
-    z = (s * (1 / step2)) + 1;
-    if s <= (1.0625 * s_max)
+    z = fix((s * (1 / step2)) + 1);
+    if s < (1.0625 * s_max)
         if s == (0.5 * s_max)
-            flos = -2 * pi * 3.6 * 10^9 *((0.9 * d_max) / 3*10^8);
             prpolos = 1 / (0.9 * d_max);
-            x = hypot((0.9 * d_max), (s_max / 2));
-            fTXsuRXn = -2 * pi * 3.6 * 10^9 * (x / 3 * 10^8);
-            prposcsu 
-            sumaprpo = prpolos;
-
-        else
-            dscn = hypot(d, n * s_max);
-            dsun = hypot(d, n * h_max);
-            dscpn = abs(((n+1) * d_max) - d);
-
-            fTXscRXn = -2 * pi * 3.6 * 10^9 * (dscn ./ 3 * 10^8); %œciana
-            fTXsuRXn = -2 * pi * 3.6 * 10^9 * (dsun ./ 3 * 10^8); %sufit
-            fTXscpRXn = -2 * pi * 3.6 * 10^9 * (dscpn ./3 * 10^8); %œciana w linii LOS
-
-            prposcn = (((a^n) ./ dscn) .* exp(-j .* fTXscRXn));
-            prposun = (((a^n) ./ dsun) .* exp(-j .* fTXsuRXn));
-            prposcpn = (((a^n) ./ dscpn) .* exp(-j .* fTXscpRXn));
-
-            sumaprpo = sumaprpo + ((2 * prposcn) + (2 * prposun) + prposcpn);
+            x = hypot((0.9 * d_max), s);
+            fTXpsRXn = -2 * pi * f * (x / c);
+            prpops = ((a / x) * exp(-j * fTXpsRXn));
+            prpo2(z) = prpolos + (2 * prpops);
+        elseif s < (0.5 * s_max)
+            los = hypot((0.9 * d_max), ((0.5 * s_max) - s));
+            prpolos = 1 / (los);
+            x = hypot((0.9 * d_max), (2 * ((0.5 * s_max) - s)));
+            fTXpsRXn = -2 * pi * f * (x / c);
+            prpops = ((a / x) * exp(-j * fTXpsRXn));
+            prpo2(z) = prpolos + (2 * prpops);
+        elseif (s > (0.5 * s_max)) & (s < (s_max))
+            los = hypot((0.9 * d_max), (s - (0.5 * s_max)));
+            prpolos = 1 / (los);
+            x = hypot((0.9 * d_max), (2 * (s - (0.5 * s_max))));
+            fTXpsRXn = -2 * pi * f * (x / c);
+            prpops = ((a / x) * exp(-j * fTXpsRXn));
+            prpo2(z) = prpolos + (2 * prpops);
+        elseif (s < (1.0625 * s_max)) & (s >= (s_max))
+            los = hypot((0.9 * d_max), (s - (0.5 * s_max)));
+            prpolos = 1 / (los);
+            x = hypot((0.9 * d_max), (2 * (s - (0.5 * s_max))));
+            fTXpsRXn = -2 * pi * f * (x / c);
+            prpops = ((a / x) * exp(-j * fTXpsRXn));
+            prpo2(z) = prpolos + prpops;
         end
+    elseif s > (1.0625 * s_max)
+        %{
+        gamma1 = hypot((0.8 * d_max), (0.5 * s_max));
+        gamma2 = hypot((0.1 * d_max), (s - s_max));
+        s1s2 = hypot((0.9 * d_max), (s - (0.5 * s_max)));
+        OBtroj = gamma1 + gamma2 + s1s2;
+        p = OBtroj / 2;
+        Ptroj = sqrt(p * (p - gamma1) * (p - gamma2) * (p - s1s2));
+        h = (2 * Ptroj) / s1s2;
+        lambda = c / f;
+        v = h * sqrt((2 / lambda) * (s1s2 / (gamma1 * gamma2)));
+        C = 6.9 + (20 * log(sqrt((v - 0.1)^2 + 1) + v - 0.1));
+        prpo2(z) = C;
+%}
+        prpo2(z) = 0;
     end
+    absprpo2(z) = (abs(prpo2(z)))^2;
 end
+
+figure
+semilogx(sx, absprpo2)
+grid
+grid minor
+title('Wzglêdny spadek mocy dla sumy œcie¿ki LOS i œcie¿ki powsta³ej na skutek jednego odbicia oraz dla cienia radarowego tylko dla dyfrakcji')
+xlabel('Szerokoœæ [m]');
+ylabel('Moc [dB]');
